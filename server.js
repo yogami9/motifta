347817bd15cc7a2c6435c6619bc28a6d2ee8c526
yogami9 <lightwyogami9@gmail.com>
2025-02-17 10:58:19 +0300

    New project created

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+const port = 3000;
 
 // Serve static files like CSS, JS, and images
 app.use(express.static(path.join(__dirname, 'public')));
